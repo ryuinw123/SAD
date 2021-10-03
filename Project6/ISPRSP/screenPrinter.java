b3b231716aad75f886ba43ad643ba2043f6f39c3
ryuinw123 <47142982+ryuinw123@users.noreply.github.com>
2021-10-03 17:04:48 +0700

    Add files via upload

---
+++ Project6/ISPRSP/screenPrinter.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.solid.book;
+
+import com.solid.book.Book.Printer;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author ryu
+ */
+public class screenPrinter implements Printer{
+    
+
+
+    @Override
+    public void printPage(Book book) {
+        boolean isEven = false;
+        String previousPage = "";
+        try {
+            FileWriter fileWriter = new FileWriter(book.getTitle() + ".txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            do {
+                if (isEven) {
+                    previousPage = book.getCurrentPage();
+                    isEven = false;
+                } else {
+                    printWriter.printf("%-25s : %25s%n", previousPage, book.getCurrentPage());
+                    isEven = true;
+                }
+            } while (book.turnToNextPage());
+            printWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Cannot print this book due to IOException");
+        }
+    }
+    
+    
+    
+}
